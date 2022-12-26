5122d659ef4fb8a9c4131b48faf98d2c92513301
katikovka <114076451+katikovka@users.noreply.github.com>
2022-12-26 14:03:59 +0300

    Add files via upload

---
+++ control2.py
@@ -0,0 +1,17 @@
+# 11. Магическая дата
+
+date = input()
+
+
+def magic_date():
+    day = int(date.split(".")[0])
+    month = int(date.split(".")[1])
+    ear = int(date.split(".")[2])
+
+    if day * month == (ear % 100):
+        print("Это магическая дата!")
+    else:
+        print("Это не магическая дата :(")
+
+
+magic_date()
